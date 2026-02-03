dbad449f0c58e5ed0530c1371b75cc5521b17db3
Linu Anil Teresa <78141612+linuteresa@users.noreply.github.com>
2026-02-03 11:23:52 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+EMAIL_SENDER = os.getenv("EMAIL_USER")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASS")
+EMAIL_RECEIVER = os.getenv("EMAIL_RECEIVER")
